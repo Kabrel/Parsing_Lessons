514916764735cce082548fc72ab14dc188ad5029
Kabrel <37102421+Kabrel@users.noreply.github.com>
2021-12-24 17:36:31 +0300

    HW_1 done!
    Homework done again. Auth made on python, without screenshots.

---
+++ 1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+switch_dict = {'1': 'Пусто',
+               '2': 'Стол совещаний',
+               '3': 'Стол сотрудника',
+               '4': 'Медиаплеер',
+               '5': 'AppleTV',
+               '6': 'Пусто'
+               }
+
+print(type(switch_dict.keys()))
+
+print(switch_dict.values())
+
