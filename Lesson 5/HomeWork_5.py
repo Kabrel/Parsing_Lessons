410f6021a799674f72d39cc078f9fbf79b1edb96
Kabrel <37102421+Kabrel@users.noreply.github.com>
2022-01-20 22:52:51 +0300

    Update HomeWork_5.py
    Done

--- Lesson 5/HomeWork_5.py
+++ Lesson 5/HomeWork_5.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
+import pymongo
+
+client = pymongo.MongoClient('localhost', 27017)
+db = client.mails_db
+mails_collection = db.mail_collection
 
 user_login = 'study.ai_172'
 user_pass = 'NextPassword172#'
@@ -21,36 +26,29 @@ login.send_keys(f"{user_login}\n")
 password = driver.find_element(By.XPATH, "//input[contains(@class, 'password-input')]")
 password.send_keys(f"{user_pass}\n")
 
-# while True:
-#     try:
-#         wait = WebDriverWait(driver, 10)
-#         cancel_btn = wait.until(EC.visibility_of_element_located((By.XPATH, "//button[contains(@data-test-id, "
-#                                                                             "'recovery-addPhone-cancel')]")))
-#         cancel_btn.click()
-#     except:
-#         break
+mails = {}
+no_mails_exist = False
 
-
-def get_url():
-    mail_list = []
-    mails = driver.find_elements(By.XPATH, "//a[contains(@class, 'js-letter-list-item')]")
-    for mail in mails:
+while not no_mails_exist:
+    mails_loaded = False
+    mails_container = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(
+        [By.XPATH, "//a[contains(@href, '/inbox/') and contains(@class, 'llc')]"]))
+    mails_size_before_add = len(mails)
+    for mail in mails_container:
         mail_url = mail.get_attribute('href')
-        mail_list.append(mail_url)
-    return mail_list
+        mails[mail_url] = {}
+    if len(mails) == mails_size_before_add:
+        no_mails_exist = True
+    mails_container[len(mails_container)//2].send_keys(Keys.PAGE_DOWN)
+    time.sleep(0.5)
 
-
-def scroll_page(site):
-    site.execute_script("window.scrollTo(0, 768)")
-
-
-mails_urls = []
-
-while True:
-    tries = 0
-    mails_urls.extend(get_url())
-    scroll_page(driver, tries, scroll_val)
-    tries += 1
-    #
-
-print()
+for url in mails.keys():
+    mail_db_elem = {}
+    driver.get(url)
+    mail_body = WebDriverWait(driver, 10).until( EC.presence_of_element_located((By.XPATH, "//div[contains(@class, "
+                                                                                           "'letter-body')]")))
+    mail_db_elem['_id'] = url
+    mail_db_elem['data'] = mail_body.text
+    mails_collection.insert_one(mail_db_elem)
+    driver.back()
+driver.close()
